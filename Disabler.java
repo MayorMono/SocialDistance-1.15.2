174508ca33451f5eaf7a5a97131beac4380b3add
MayorGraphite <48899747+MayorGraphite@users.noreply.github.com>
2020-05-05 15:15:08 -0700

    Added invalid argument handling

--- Disabler.java
+++ Disabler.java
@@ -9,10 +9,13 @@ public class Disabler implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		if(Main.enabled && arg0.isOp()) {
-			Main.enabled = false;
-			Bukkit.broadcastMessage("The pandemic has ended");
+		if(arg3.length == 0) {
+			if(Main.enabled && arg0.isOp()) {	
+				Main.enabled = false;
+				Bukkit.broadcastMessage("The pandemic has ended");
+				return true;
+			}
 		}
-		return true;
+		return false;
 	}
 }
