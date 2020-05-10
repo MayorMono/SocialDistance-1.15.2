d55db9a0e416d22f707ea6ab1723082a92bc5c34
MayorMono <48899747+MayorMono@users.noreply.github.com>
2020-05-10 14:12:55 -0700

    Improved readability of output

--- src/com/meeku/socialDistance/Reporter.java
+++ src/com/meeku/socialDistance/Reporter.java
@@ -1,25 +1,31 @@
-package com.meeku.socialDistance;
+package com.meeku.socialdistance;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class Reporter implements CommandExecutor {
-
+	
+	// Reports status of pandemic
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
 		if(arg3.length == 0) {
-			if(Main.enabled && arg0.isOp()) {
-				Bukkit.broadcastMessage("There is currently a pandemic. "
-						+ "The danger radius is " + Main.radius + 
-						". The infection period is " + Main.duration + " seconds."
-								+ " The amplifier is " + Main.amplifier);
-				return true;
-			} else if(arg0.isOp()) {
-				Bukkit.broadcastMessage("No pandemic is occuring");
-				return true;
+			
+			arg0.sendMessage("--------------------------------");
+			
+			if(SocialDistance.enabled) {
+				arg0.sendMessage("There is currently a pandemic");
+			} else {
+				arg0.sendMessage("No pandemic is occuring");
 			}
+			
+			arg0.sendMessage("Mobs infected: " + SocialDistance.mobsInfected);
+			arg0.sendMessage("Danger distance: " + SocialDistance.radius);
+			arg0.sendMessage("Infection period: " + SocialDistance.duration + " second(s)");
+			arg0.sendMessage("Amplifier: " + SocialDistance.amplifier);
+			arg0.sendMessage("--------------------------------");
+				
+			return true;
 		}
 		
 		return false;
