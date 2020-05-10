76bab52725ef49a47f1ebbe6e800d5643cac0e84
MayorMono <48899747+MayorMono@users.noreply.github.com>
2020-05-10 14:12:22 -0700

    Added description, increased maximum radius

--- src/com/meeku/socialDistance/RadiusSetter.java
+++ src/com/meeku/socialDistance/RadiusSetter.java
@@ -1,4 +1,4 @@
-package com.meeku.socialDistance;
+package com.meeku.socialdistance;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -6,21 +6,24 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class RadiusSetter implements CommandExecutor {
-
+	
+	// Sets danger radius to argument if argument is in range [1,127]
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
 		if(arg0.isOp()) {
+			
+			if(!(arg3.length == 1)) {
+				return false;
+			}
+			
 			try {
-				if(!(arg3.length == 1)) {
-					return false;
-				}
-				
 				int radius = Integer.parseInt(arg3[0]);
 				
-				if(radius > 0 && radius <= 10) {
-					Main.radius = radius;
+				if(radius > 0 && radius <= 127) {
+					SocialDistance.radius = radius;
 					Bukkit.broadcastMessage("Danger radius is now " + arg3[0]);
 				}
+			
 			} catch(NumberFormatException e) {
 				return false;
 			} catch(IndexOutOfBoundsException e) {
