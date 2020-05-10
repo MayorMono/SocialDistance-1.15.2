1387ad9bf0939bd839ef6865ada9060c7a0d8b4d
MayorMono <48899747+MayorMono@users.noreply.github.com>
2020-05-10 14:11:05 -0700

    Added description, increased max duration

--- src/com/meeku/socialDistance/DurationSetter.java
+++ src/com/meeku/socialDistance/DurationSetter.java
@@ -1,4 +1,4 @@
-package com.meeku.socialDistance;
+package com.meeku.socialdistance;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -6,21 +6,24 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class DurationSetter implements CommandExecutor {
-
+	
+	// Sets potion effect duration to command argument (in seconds) if argument is in range [1,1200]
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
 		if(arg0.isOp()) {
+			
+			if(!(arg3.length == 1)) {
+				return false;
+			}
+			
 			try {
-				if(!(arg3.length == 1)) {
-					return false;
-				}
-				
 				int duration = Integer.parseInt(arg3[0]);
 				
-				if(duration > 0 && duration <= 60) {
-					Main.duration = duration;
+				if(duration > 0 && duration <= 1200) {
+					SocialDistance.duration = duration;
 					Bukkit.broadcastMessage("Infection duration is now " + arg3[0] + " seconds");
 				}
+			
 			} catch(NumberFormatException e) {
 				return false;
 			} catch(IndexOutOfBoundsException e) {
