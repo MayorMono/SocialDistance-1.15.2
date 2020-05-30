9f65b1756120ef155b29a051f609862bb0e15796
MayorMono <48899747+MayorMono@users.noreply.github.com>
2020-05-29 22:47:05 -0700

    Refactored
    Refactored run method in scheduleSyncRepeatingTask call into a separate private method

--- src/com/meeku/socialdistance/SocialDistance.java
+++ src/com/meeku/socialdistance/SocialDistance.java
@@ -22,8 +22,7 @@ public class SocialDistance extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		System.out.println("Social Distance by MayorMono");
-		
-		// Register commands
+
 		this.getCommand("penable").setExecutor(new Enabler());
 		this.getCommand("pdisable").setExecutor(new Disabler());
 		this.getCommand("pstatus").setExecutor(new Reporter());
@@ -31,62 +30,50 @@ public class SocialDistance extends JavaPlugin {
 		this.getCommand("psetduration").setExecutor(new DurationSetter());
 		this.getCommand("psetamplifier").setExecutor(new AmplifierSetter());
 		this.getCommand("ptogglemobs").setExecutor(new MobToggler());
-		
-		// Register scheduler that checks for distance violations every in-game tick (0.05s)
+
 		BukkitScheduler scheduler = getServer().getScheduler();
-		
-		// Scheduler runs the run method every tick if a pandemic is enabled
-		scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
-			@Override
-			public void run() {
-				if(enabled) {
-					
-					// Generate potion effect for current tick
-					PotionEffect virus = new PotionEffect(PotionEffectType.WITHER, duration * 20, amplifier);
-					
-					// Iterate through list of online players (not in SPECTATOR mode) to check for distance violations
-					for(Player p1 : Bukkit.getOnlinePlayers()) {
-						
-						if(!(p1.getGameMode() == GameMode.SPECTATOR)) {
-							
-							// Iterate through entities within a cube with length radius*2 around p1 (player 1)
-							List<Entity> near = p1.getNearbyEntities(radius,radius,radius);
-							for(Entity e : near) {
-								
-								/*
-								 * If e is a player not in SPECTATOR mode, calculate distance squared between p1 and e (casted to p2).
-								 * If distance squared < radius^2, give both players virus potion effect.
-								 */
-								if(e instanceof Player) {
-									Player p2 = (Player) e;
-									
-									Location p1L = p1.getLocation();
-									Location p2L = p2.getLocation();
-									
-									if(!(p2.getGameMode() == GameMode.SPECTATOR) && (p1L.distanceSquared(p2L) < (radius * radius))) {
-										p1.addPotionEffect(virus);
-										p2.addPotionEffect(virus);
-									}	
-									
-								/*
-								 * Else if e is a non-player mob, and mobs are infected, calculate distance squared between 
-								 * p1 and e. If distance squared < radius^2, give p1 virus potion effect.
-								 */
-								} else if(e instanceof Mob && mobsInfected) {
-									Location p1L = p1.getLocation();
-									Location eL = e.getLocation();
-									
-									if(p1L.distanceSquared(eL) < (radius * radius)) {
-										p1.addPotionEffect(virus);
-									}		
-								}
-							
+		scheduler.scheduleSyncRepeatingTask(this, this::infect, 0L, 1L);
+	}
+
+	// Iterates through list of online players in search of social distancing violations
+	private void infect() {
+		if(enabled) {
+
+			// Generate potion effect for current tick
+			PotionEffect virus = new PotionEffect(PotionEffectType.WITHER, duration * 20, amplifier);
+
+			for(Player p1 : Bukkit.getOnlinePlayers()) {
+
+				if(!(p1.getGameMode() == GameMode.SPECTATOR)) {
+
+					List<Entity> near = p1.getNearbyEntities(radius,radius,radius);
+					for(Entity e : near) {
+
+						if(e instanceof Player) {
+							Player p2 = (Player) e;
+
+							Location p1L = p1.getLocation();
+							Location p2L = p2.getLocation();
+
+							if(!(p2.getGameMode() == GameMode.SPECTATOR) && (p1L.distanceSquared(p2L) < (radius * radius))) {
+								p1.addPotionEffect(virus);
+								p2.addPotionEffect(virus);
+							}
+
+						} else if(e instanceof Mob && mobsInfected) {
+							Location p1L = p1.getLocation();
+							Location eL = e.getLocation();
+
+							if(p1L.distanceSquared(eL) < (radius * radius)) {
+								p1.addPotionEffect(virus);
 							}
 						}
-					
+
 					}
 				}
+
 			}
-		}, 0L, 1L);
+		}
 	}
+
 }
